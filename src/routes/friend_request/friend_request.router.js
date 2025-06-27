e872c66acb6036538c8f619a93da54b27e8e4d3c
Leonardo Rodrigues <leorgois@hotmail.com>
2025-06-27 08:31:17 -0300

    Revert "Rota de amigos CRUD"
    This reverts commit 8afaf916e17e34b27c25933631b3672228073b1d.

--- src/routes/friend_request/friend_request.router.js
+++ src/routes/friend_request/friend_request.router.js
@@ -3,8 +3,5 @@ const controller = require('./friend_request.controller');
 const friendRequestRouter = express.Router();
 
 friendRequestRouter.post('/friendRequest', controller.friendRequest);
-friendRequestRouter.get('/getFriends', controller.getFriends);
-friendRequestRouter.get('/getRequests', controller.getRequests);
-friendRequestRouter.get('/getSends', controller.getSends);
 
 module.exports = friendRequestRouter;
