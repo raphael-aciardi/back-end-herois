925235d8bab5aa49e507f0961fc780a09e68628e
Leonardo Rodrigues <leorgois@hotmail.com>
2025-06-27 08:31:25 -0300

    Revert "a"
    This reverts commit 64c7b6729b91f601e42de6c0b5f2b5e1697841db.

--- src/routes/friend_request/friend_request.router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const controller = require('./friend_request.controller');
-const friendRequestRouter = express.Router();
-
-friendRequestRouter.post('/friendRequest', controller.friendRequest);
-
-module.exports = friendRequestRouter;
